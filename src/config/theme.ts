75b32b640d644ed27cb9422001d0e349ff57a4ac
Mauricio Martinez <maumarcastil@gmail.com>
2023-04-02 21:49:31 -0500

    feat: define colour palette for the theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,19 +1,32 @@
 const theme = {
     light: {
         textPrimary: '#0DA54B',
-        greenPrimary: '#0DA54B',
-        greenSecondary: '#097334',
-        grayPrimary: '#999EA1',
+        textSecondary: '#999EA1',
+        textBlackAndWhite: '#000000',
         borderInput: '#898e91',
+
+        backgroundCard: '#ffffff',
+        backgroundImage: '#F0F0F0',
+        backgroundQuantity: '#EAEAEA',
+        backgroundModal: '#FFFFFF',
+
 
 
         textGrayPrimary: '#999EA1',
         textGraySecondary: '#6b6e71',
+        textRedPrimary: '#D82F2F',
+
+        /* colors */
+        greenPrimary: '#0DA54B',
+        greenSecondary: '#097334',
+        grayPrimary: '#999EA1',
+        redPrimary: '#D82F2F',
+        redSecondary: '#ac2525',
+
+
+
 
         border: '#898e91',
-
-
-
 
         text: '#0DA54B',
         text2: '#999EA1',
@@ -24,6 +37,8 @@ const theme = {
 
         /*  */
         background: '#fff',
+
+
         background2: '#FFFFFF',
         firstText: '#000000',
         secondText: '#0E1621',
@@ -32,19 +47,32 @@ const theme = {
     },
     dark: {
         textPrimary: '#ffffff',
-        greenPrimary: '#0DA54B',
-        greenSecondary: '#097334',
-        grayPrimary: '#999EA1',
+        textSecondary: '#ffffff',
+        textBlackAndWhite: '#FFFFFF',
         borderInput: '#EAEAEA',
+
+        backgroundCard: '#0E1723',
+        backgroundImage: '#0E1723',
+        backgroundQuantity: '#152437',
+        backgroundModal: '#152437',
 
 
         textGrayPrimary: '#999EA1',
         textGraySecondary: '#ffffff',
+        textRedPrimary: '#D82F2F',
+
+
+        /* colors */
+        greenPrimary: '#0DA54B',
+        greenSecondary: '#097334',
+        grayPrimary: '#999EA1',
+        redPrimary: '#D82F2F',
+        redSecondary: '#ac2525',
+
+
+
 
         border: '#EAEAEA',
-
-
-
 
         text: '#ffffff',
         text2: '#999EA1',
@@ -56,6 +84,9 @@ const theme = {
 
         /*  */
         background: '#152437',
+
+
+
         background2: '#1A1F27',
         firstText: '#E2E5E9',
         secondText: '#AFC0DF',
